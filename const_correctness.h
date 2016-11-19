e040e8aede775cced38ae6b70b0d51ad73ddd125
Milos Zivkovic <zivkovicmilos5@hotmail.com>
2016-11-19 14:23:22 +0100

    const correctness test stillt

--- const_correctness.h
+++ const_correctness.h
@@ -2,6 +2,23 @@ class T {
 	char* t;
 
 public:
+	/*INSPEKTORI I MUTATORI*/
+	//INSPEKTORI IMAJU CONST IZA DEKLARACIJE 
+
+	void inspect() const;   // This member promises NOT to change *this
+	void mutate() /*const - ako se doda nece da bude prob*/;     
+	// This member function might change *this
+
+
+	void userCode(T& changeable, const T& unchangeable)
+	{
+		changeable.inspect();   // Okay: doesn't change a changeable object
+		changeable.mutate();    // Okay: changes a changeable object
+		unchangeable.inspect(); // Okay: doesn't change an unchangeable object
+		//unchangeable.mutate();  // ERROR: attempt to change unchangeable object
+	}
+
+
 	void g1(char*& x);
 	void f1(const char*& y) {
 		//g1(y);
@@ -19,6 +36,19 @@ public:
 	/* const X* const p := -||- objekat klase X je konstantan nista nije moguce
 	promeniti. Citati sdesna na levo! 
 	*/
+
+	/*
+	const X& x := referenca x na objekat klase X koji je konstantan
+	
+	*/
+
+	/*
+	X const& x =  const X& x
+	*/
+	/*
+	X& const x nema smisla reference su uvek konstante
+	*/
+
 
 
 
