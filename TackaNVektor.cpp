12aa81af6d09cdb7d2b2059a0486e041e5243f20
Milos Zivkovic <zivkovicmilos5@hotmail.com>
2016-11-19 11:05:55 +0100

    prefix end

--- TackaNVektor.cpp
+++ TackaNVektor.cpp
@@ -15,15 +15,19 @@ void Vektor::setV(int k) {
 
 Vektor& Vektor::operator++(int) {//postfix
 	Tacka tmp;
-
+	int v = getV();
 	tmp = niz[v]; //pomocni
 	for (int i = v; i >= 0; i--)
 		niz[i] = niz[i - 1];
 	niz[0] = tmp;
 }
 
-Vektor& Vektor::operator++() {//postfix
+Vektor& Vektor::operator++() {//prefix
+	Tacka tmp;
+	int v = getV();
 
-
-
+	tmp = niz[0];
+	for (int i = 0; i < v; i++)
+		niz[i] = niz[i + 1];
+	niz[v] = tmp;
 }
