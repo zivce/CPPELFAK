52b7c18a96fcd955d802fd4ddcabec5cab8208c5
Milos Zivkovic <zivkovicmilos5@hotmail.com>
2016-11-19 09:36:52 +0100

    tacka.h #1

--- Tacka.h
+++ Tacka.h
@@ -1,6 +1,20 @@
 #ifndef TACKA_H_
 #define TACKA_H_
+class Tacka {
+	double x;
+	double y;
+	double z;
+public:
+	Tacka& operator+ (Tacka& X);
 
+	Tacka& operator- (Tacka& X);
+
+	friend Tacka& operator<< (istream& ulaz, Tacka& X);
+
+	friend Tacka& operator>> (ostream& izlaz, Tacka& X);
+
+
+};
 
 
 
