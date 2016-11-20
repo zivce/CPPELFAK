59274dddb7e60a7e54547c9a205d39ac22b69e6c
Milos Zivkovic <zivkovicmilos5@hotmail.com>
2016-11-20 09:58:30 +0100

    polinom h

--- Polinom.h
+++ Polinom.h
@@ -1,1 +1,21 @@
 #pragma once
+#include <iostream>
+class Polinom {
+private:
+	int V;//stepen polinoma
+	double* niz; //koeficijenti
+	float arg;
+
+public:
+	void setArg(float arg);
+	Polinom();
+	Polinom(int);
+	~Polinom();
+	Polinom(const Polinom& V);
+
+	Polinom& operator=(const Polinom& v);
+
+
+
+
+};
