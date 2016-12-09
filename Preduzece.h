3b17e524edc8b109ab6c99e4d20a643194f77ed6
Milos Zivkovic <zivkovicmilos5@hotmail.com>
2016-12-09 21:21:32 +0100

    radnik.h started

--- Preduzece.h
+++ Preduzece.h
@@ -1,1 +1,14 @@
 #pragma once
+
+#include "Radnik.h"
+
+class Preduzece {
+private:
+	char* naziv;
+	int budget;
+	//Radnik* niz;
+	int maxCountNiz;
+	int countNiz;
+public:
+
+};
--- Radnik.h
+++ Radnik.h
@@ -1,1 +1,35 @@
 #pragma once
+class Radnik {
+protected:
+	int JMBG;
+	char* imePrezime;
+	int yearEmp;
+	int yearOld;
+	int osnPlate;
+public:
+	inline int getJmbg() {
+		return this->JMBG;
+	}
+
+	inline char* getImePrezime() {
+		return this->imePrezime;
+	}
+
+	inline int getYearEmp() {
+		return this->yearEmp;
+	}
+	virtual int plataRadnika(int)=0;
+
+	int operator++(int);
+
+	int operator--(int);
+
+
+
+	Radnik& operator- (const Radnik&); //cmp
+	
+	
+	virtual void printData() = 0;
+
+
+};
